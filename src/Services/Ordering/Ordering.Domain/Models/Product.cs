946672bf484e07fe3cd7cb87767cb776858cb34a
Ruslan.Galandarli <ruslan.galandarli@gmail.com>
2026-01-06 18:08:12 +0400

    ref

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,5 +1,4 @@
-﻿
-using Ordering.Domain.Abstractions;
+﻿using Ordering.Domain.Abstractions;
 using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Domain.Models
@@ -9,6 +8,19 @@ namespace Ordering.Domain.Models
         public string Name { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
 
+        public static Product Create(ProductId productId,string name,decimal price)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var product = new Product()
+            {
+                Id = productId,
+                Name = name,
+                Price = price
+            };
+            return product;
+        }
        
     }
 
